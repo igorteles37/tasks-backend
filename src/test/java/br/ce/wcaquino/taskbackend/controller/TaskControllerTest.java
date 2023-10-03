27ba36be9e74267433ebec2b226a3fe38ed159bb
Igor Teles Oliveira <igor.teles@gmail.com>
2023-10-02 22:15:30 -0300

    Melhorar testes com assert fail

--- src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -1,6 +1,7 @@
 package br.ce.wcaquino.taskbackend.controller;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
@@ -20,34 +21,33 @@ import br.ce.wcaquino.taskbackend.model.Task;
 import br.ce.wcaquino.taskbackend.repo.TaskRepo;
 import br.ce.wcaquino.taskbackend.utils.ValidationException;
 
-
 //@RunWith(MockitoJUnitRunner.class)
 public class TaskControllerTest {
-	
+
 	@InjectMocks
 	private TaskController taskControler;
 
 	@Mock
 	private TaskRepo taskRepoMock;
-	
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
-	public void naoDeveSalvarTarefaSemDescricao()  {
+	public void naoDeveSalvarTarefaSemDescricao() {
 		Task task = new Task();
 		task.setDueDate(LocalDate.now());
 		task.setId(1L);
 		try {
 			taskControler.save(task);
+			fail("Não deveria salvar");
 		} catch (ValidationException e) {
 			assertEquals("Fill the task description", e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void naoDeveSalvarTarefaSemData() {
 		Task task = new Task();
@@ -55,12 +55,13 @@ public class TaskControllerTest {
 		task.setId(1L);
 		try {
 			taskControler.save(task);
+			fail("Não deveria salvar");
 		} catch (ValidationException e) {
 			assertEquals("Fill the due date", e.getMessage());
 		}
-		
+
 	}
-	
+
 	@Test
 	public void naoDeveSalvarTarefaComDataPassada() {
 		Task task = new Task();
@@ -69,26 +70,24 @@ public class TaskControllerTest {
 		task.setId(1L);
 		try {
 			taskControler.save(task);
+			fail("Não deveria salvar");
 		} catch (ValidationException e) {
 			assertEquals("Due date must not be in past", e.getMessage());
 		}
-		
+
 	}
-	
+
 	@Test
 	public void deeSalvarTarefaComSucesso() throws ValidationException {
-		
+
 		Task task = new Task();
 		task.setTask("descricao");
 		task.setDueDate(LocalDate.now().plusDays(1));
 
-		
 		when(taskRepoMock.save(task)).thenReturn(task);
-		
 		ResponseEntity<Task> taskSaved = taskControler.save(task);
-		
+
 		Mockito.verify(taskRepoMock, times(1)).save(task);
-		
 		assertEquals(task.getTask(), taskSaved.getBody().getTask());
 	}
 
